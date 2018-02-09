3fa7492f8346f20cba00eb5e73f098229cda1f5f
botter <xiaobao@inspiry.cn>
2018-02-09 15:20:11 +0800

    add mvn repackage env
    Signed-off-by: botter <xiaobao@inspiry.cn>

---
+++ nest-security/src/main/java/com/nest/security/DataConfig.java
@@ -0,0 +1,34 @@
+package com.nest.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "datasource.master")
+public class DataConfig {
+
+    private String username;
+
+    private String password;
+
+    DataConfig(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- nest-security/src/main/java/com/nest/security/SecurityServiceApplication.java
+++ nest-security/src/main/java/com/nest/security/SecurityServiceApplication.java
@@ -1,6 +1,8 @@
 package com.nest.security;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,12 +13,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SecurityServiceApplication {
+public class SecurityServiceApplication implements CommandLineRunner{
+
+	@Autowired
+	private DataConfig dataConfig;
 
 	public static void main(String[] args) {
 		IFunctionInterface<String, String> fi1 = (o) -> "hello1 function interface annotation" + o;
 		System.out.println(fi1.test("000"));
 		SpringApplication.run(SecurityServiceApplication.class, args);
 
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println(dataConfig.getPassword());
 	}
 }
