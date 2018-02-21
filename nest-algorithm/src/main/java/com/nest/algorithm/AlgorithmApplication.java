8b67423203a8c4869913b5d445aa2b009dfe5b27
botter <xiaobao@inspiry.cn>
2018-02-21 11:46:27 +0800

    add Algorithm  module
    Signed-off-by: botter <xiaobao@inspiry.cn>

---
+++ nest-algorithm/src/main/java/com/nest/algorithm/AlgorithmApplication.java
@@ -0,0 +1,16 @@
+package com.nest.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * 算法 module
+ * @author botter
+ */
+@SpringBootApplication
+public class AlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmApplication.class, args);
+	}
+}
